fd0fab3dabf00a457382f63012e80cccc5b449e3
Lorety-VL <vladimirking18@gmail.com>
2024-04-27 18:16:54 +0300

    step 4

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Cli {
     public static void greeting() {
+        System.out.println("Welcome to the Brain Games!");
         var sc = new Scanner(System.in);
         System.out.print("May I have your name? ");
         var name = sc.next();
